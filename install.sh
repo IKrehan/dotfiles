c316d232d5c8786ded5d99a6f744f07395d5dd2e
Irvig Krehan <irvigkrehan@hotmail.com>
2023-02-18 14:57:32 -0300

    feat: install base dependencies

---
+++ install.sh
@@ -0,0 +1,9 @@
+# Update
+sudo apt update & sudo apt upgrade
+
+# Install Base Packages
+sudo apt install curl git vim i3 polybar feh -y
+
+# Install alacritty
+sudo add-apt-repository ppa:aslatter/ppa -y
+sudo apt install alacritty -y
