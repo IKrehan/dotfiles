18826eb348cf1e2382ea1389be2a0e8c2fabadf2
Irvig Krehan <irvigkrehan@hotmail.com>
2023-02-19 01:05:07 -0300

    feat: visual rice

--- install.sh
+++ install.sh
@@ -2,16 +2,26 @@
 sudo apt update & sudo apt upgrade
 
 # Install Base Packages
-sudo apt install curl git vim -y
+sudo apt install curl git neovim tmux -y
 
 # Install Window Manager
-# Getting alternative repository because ie 4.22 is not on officil ubuntu repo yet
+# Getting alternative repository because i3 4.22 is not yet on official ubuntu repo
 /usr/lib/apt/apt-helper download-file https://debian.sur5r.net/i3/pool/main/s/sur5r-keyring/sur5r-keyring_2023.02.18_all.deb keyring.deb SHA256:a511ac5f10cd811f8a4ca44d665f2fa1add7a9f09bef238cdfad8461f5239cc4
 sudo apt install ./keyring.deb -y
 echo "deb http://debian.sur5r.net/i3/ $(grep '^DISTRIB_CODENAME=' /etc/lsb-release | cut -f2 -d=) universe" | sudo tee /etc/apt/sources.list.d/sur5r-i3.list
 sudo apt update -y
-sudo apt install i3 polybar feh -y
+sudo apt install i3 polybar picom -y
 
 # Install alacritty
 sudo add-apt-repository ppa:aslatter/ppa -y
 sudo apt install alacritty -y
+
+# Install ZSH
+sudo apt install zsh -y
+sudo chsh -s $(which zsh)
+
+# Install Antigen (Oh My ZSH)
+mkdir ~/.antigen
+curl -L git.io/antigen > ~/.antigen/antigen.zsh
+
+
